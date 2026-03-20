0e9150aed0f249c021e6a0484e2e6316b0742637
SpertixMato <mato@spertix.com>
2026-03-20 22:43:19 +0100

    update charachter movment

--- Assets/Scripts/CharacterControllerMovement.cs
+++ Assets/Scripts/CharacterControllerMovement.cs
@@ -7,11 +7,15 @@ public class CharacterControllerMovement : MonoBehaviour
     [SerializeField] private Transform _camera;
     [SerializeField] private float _movementSpeed;
     [SerializeField] private float _jumpVelocity;
+    [SerializeField] private float _directionalRotationSpeed;
+    [SerializeField] private float _slideFriction;
 
-    private const float GRAVITY = -9.81f;
-    private const float GROUNDED_GRAVITY_PULL = -0.1f;
+    private const float GRAVITY = -19.62f;
+    private const float GROUNDED_GRAVITY_PULL = -0.5f;
     private Vector3 _velocity;
     private bool _hasJumped;
+    private Vector3 _hitNormal;
+    private bool _onSteepSlope;
 
     void Start()
     {
@@ -25,38 +29,56 @@ public class CharacterControllerMovement : MonoBehaviour
 
     void Update()
     {
+        _onSteepSlope = Vector3.Angle(Vector3.up, _hitNormal) > _characterController.slopeLimit;
+
         Gravity();
 
-        Vector3 directionF = _camera.forward * _movementSpeed * InputManager.Instance.VerticalAxis * Time.deltaTime;
-        Vector3 directionH = _camera.right * _movementSpeed * InputManager.Instance.HorizontalAxis * Time.deltaTime;
+        Vector3 directionF = _camera.forward * _movementSpeed * InputManager.Instance.VerticalAxis;
+        Vector3 directionH = _camera.right * _movementSpeed * InputManager.Instance.HorizontalAxis;
         directionF.y = 0;
         directionH.y = 0;
 
-        var move = directionF + directionH + _velocity;
+        Vector3 moveDir = _onSteepSlope ? Vector3.zero : directionF + directionH;
+
+        if (moveDir.sqrMagnitude > 0.01f)
+        {
+            Quaternion targetRot = Quaternion.LookRotation(moveDir.normalized);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRot, Time.deltaTime * _directionalRotationSpeed);
+        }
+
+        Vector3 move = moveDir * Time.deltaTime;
+        move.y = _velocity.y * Time.deltaTime;
+
+        if (!(Vector3.Angle(Vector3.up, _hitNormal) <= _characterController.slopeLimit))
+        {
+            Vector3 slideDir = Vector3.ProjectOnPlane(Vector3.down, _hitNormal).normalized;
+            move.x += slideDir.x * _slideFriction * Time.deltaTime;
+            move.z += slideDir.z * _slideFriction * Time.deltaTime;
+        }
 
         _characterController.Move(move);
+        _hasJumped = false;
     }
-
-    private void LateUpdate()
+    void OnControllerColliderHit(ControllerColliderHit hit)
     {
-        transform.rotation = Quaternion.Euler(0, _camera.rotation.eulerAngles.y, 0);
+        _hitNormal = hit.normal;
     }
 
     private void Gravity()
     {
-        if (_characterController.isGrounded)
+        if (_characterController.isGrounded && !_onSteepSlope)
         {
             if (_hasJumped)
             {
-                _velocity = _jumpVelocity * Vector3.up * Time.deltaTime;
+                _velocity.y = _jumpVelocity;
                 _hasJumped = false;
             }
             else
-                _velocity = Vector3.up * GROUNDED_GRAVITY_PULL;
+                _velocity.y = GROUNDED_GRAVITY_PULL;
         }
         else
         {
-            _velocity += Vector3.up * GRAVITY * Time.deltaTime * Time.deltaTime;
+            _velocity.y += GRAVITY * Time.deltaTime;
         }
     }
 }
